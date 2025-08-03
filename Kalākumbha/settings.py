105cba9c8b4704d46e690aa46b893fd6fea6afd2
Polar6ear <neuronnitin@gmail.com>
2025-08-03 16:18:30 +0530

    django authentication

--- Kalākumbha/settings.py
+++ Kalākumbha/settings.py
@@ -135,3 +135,16 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'neuralnitin@gmail.com'
+EMAIL_HOST_PASSWORD = 'xmfqchadgsrptoox'
