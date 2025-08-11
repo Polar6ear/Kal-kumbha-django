051161e5e7af9bf2994fd197fdc518c3a4504578
Polar6ear <neuronnitin@gmail.com>
2025-08-11 11:47:09 +0530

    review and rating system

--- store/models.py
+++ store/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from category.models import Category
 from django.urls import reverse
+from accounts.models import Account
+from django.db.models import Avg, Count
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
-    images = models.ImageField(upload_to="photo/products")
+    images = models.ImageField(upload_to="photo/products", null=True, blank=True)
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -18,7 +20,19 @@ class Product(models.Model):
         return reverse('product_detail', args=[self.category.slug, self.slug])
     def __str__(self):
         return self.product_name
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
 
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager, self).filter(variation_category='color', is_active=True)
@@ -40,3 +54,17 @@ class Variation(models.Model):
     objects = VariationManager()
     def __str__(self):
         return self.variation_value
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=100, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
