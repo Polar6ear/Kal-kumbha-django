df0cba254f0cebe642ad21218a3574071ed6ed28
Polar6ear <neuronnitin@gmail.com>
2025-08-16 21:11:40 +0530

    pokestore

--- accounts/views.py
+++ accounts/views.py
@@ -59,6 +59,7 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
+
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
@@ -66,46 +67,58 @@ def login(request):
                 cart = Cart.objects.get(cart_id=_cart_id(request))
                 is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exists:
-                    cart_items = CartItem.objects.filter(cart=cart)
-                    product_variation = [list(item.variations.all()) for item in cart_items]
+                    cart_item = CartItem.objects.filter(cart=cart)
 
-                    user_cart_items = CartItem.objects.filter(user=user)
-                    existing_var_list = [list(item.variations.all()) for item in user_cart_items]
-                    ids = [item.id for item in user_cart_items]
+                    # Getting the product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1, 2, 3, 4, 6]
+                    # ex_var_list = [4, 6, 3, 5]
 
                     for pr in product_variation:
-                        if pr in existing_var_list:
-                            index = existing_var_list.index(pr)
-                            item_id = ids[index]
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
                             item = CartItem.objects.get(id=item_id)
                             item.quantity += 1
                             item.user = user
                             item.save()
                         else:
-                            for item in cart_items:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
                                 item.user = user
                                 item.save()
-            except Cart.DoesNotExist:
+            except:
                 pass
-
             auth.login(request, user)
             messages.success(request, 'You are now logged in.')
-
             url = request.META.get('HTTP_REFERER')
-            if url:
-                try:
-                    query = requests.utils.urlparse(url).query
-                    params = dict(x.split('=') for x in query.split('&'))
-                    if 'next' in params:
-                        return redirect(params['next'])
-                except:
-                    pass
-            return redirect('dashboard')
+            try:
+                query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
-
     return render(request, 'accounts/login.html')
+
 
 
 @login_required(login_url='login')
--- store/views.py
+++ store/views.py
@@ -10,25 +10,32 @@ from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+
 def store(request, category_slug=None):
     categories = None
     products = None
 
-    if category_slug != None:
+    if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
-        product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        paginator = Paginator(products, 4)
-        page = request.GET.get("page")
-        paged_products = paginator.get_page(page)
-        product_count = products.count()
+
+    # ✅ Pagination dono case me apply karo
+    paginator = Paginator(products, 4)
+    page = request.GET.get("page")
+    paged_products = paginator.get_page(page)
+    product_count = products.count()
+
     context = {
         "products": paged_products,
         "product_count": product_count,
     }
     return render(request, 'store/store.html', context)
+
 
 def product_detail(request, category_slug, product_slug):
     try:
