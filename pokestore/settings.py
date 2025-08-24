e6a0b8544d6d57e161c670378636647c7387dea0
ICE BEAR <icebear@hiroshis-Mac-mini.local>
2025-08-24 22:01:55 +0530

    elastic bean aws

--- pokestore/settings.py
+++ pokestore/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.elasticbeanstalk.com', '*'] 
 
 
 # Application definition
